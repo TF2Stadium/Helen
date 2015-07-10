411bbea66c1b849a76c8dbaa43a5baef40d78f62
Vibhav Pant <vibhavp@gmail.com>
2015-07-10 21:59:59 +0530

    Move send* function to controleerHelpers.go

--- controllers/controllerHelpers.go
+++ controllers/controllerHelpers.go
@@ -7,6 +7,41 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+//Response structure
+//github.com/TeamPlayTF/Specifications/blob/master/Communication.md#response-format
+type Response struct {
+	Successful bool        `json:"successful"` //true if operation was successful
+	Data       interface{} `json:"data"`       //response message, if any
+	Code       int         `json: "code"`      //errcode, if sucessful == false
+}
+
+func SendJSON(w http.ResponseWriter, j string) {
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprintf(w, j)
+}
+
+func SendError(w http.ResponseWriter, code int, message string) string {
+	r := &Response{
+		Successful: false,
+		Data:       data,
+		Code:       code,
+	}
+	j, _ := json.Marshall(r)
+	sendJSON(w, j)
+	return string(j)
+}
+
+func SendSuccess(w http.ResponseWriter, data interface{}) string {
+	r := &Response{
+		Successful: true,
+		Data:       data,
+		Code:       -1,
+	}
+	j, _ := json.Marshall(r)
+	sendJSON(w, j)
+	return string(j)
+}
+
 func isLoggedIn(r *http.Request) bool {
 	session, _ := config.CookieStore.Get(r, config.Constants.SessionName)
 
