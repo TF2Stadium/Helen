051f65a1c1e1bed64ecee88997cab47723543d9b
Vibhav Pant <vibhavp@gmail.com>
2015-07-10 23:25:27 +0530

    remove unrequired Response struct

--- controllers/controllerHelpers.go
+++ controllers/controllerHelpers.go
@@ -8,14 +8,6 @@ import (
 	"github.com/gorilla/sessions"
 )
 
-//Response structure
-//github.com/TeamPlayTF/Specifications/blob/master/Communication.md#response-format
-type Response struct {
-	Successful bool        `json:"successful"` //true if operation was successful
-	Data       interface{} `json:"data"`       //response message, if any
-	Code       int         `json: "code"`      //errcode, if sucessful == false
-}
-
 func SendJSON(w http.ResponseWriter, json simplejson.Json) {
 	w.Header().Add("Content-Type", "application/json")
 	fmt.Fprintf(w, string(json))
