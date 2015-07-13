3324c93c4dedf2dbabe73c64c1ab460715dbbe7e
Sk1LLb0X <sk1llb0x123@gmail.com>
2015-07-13 19:34:10 -0300

    fixed a typo

--- models/lobby/server_test.go
+++ models/lobby/server_test.go
@@ -9,7 +9,7 @@ import (
 
 // change this if you wanna test the server
 // make sure you have the server running at the moment
-var shouldTest bool = true
+var shouldTest bool = false
 var svr *lobby.Server
 
 func TestServerSetup(t *testing.T) {
@@ -23,7 +23,7 @@ func TestServerSetup(t *testing.T) {
 		svr.League = "ugc"
 		svr.LobbyId = 0
 		svr.Address = "192.168.1.94:27015"
-		svr.RconPassword = "30035"
+		svr.RconPassword = "rconPassword"
 		svr.LobbyPassword = "12345"
 		svr.AllowPlayer("[U:1:106866319]") // your steamId, so it wont be kicking you out everytime
 		svr.Setup()
