79f20d7e053b7d003345efff684b43b28b28d299
Sk1LLb0X <sk1llb0x123@gmail.com>
2015-07-15 21:21:49 -0300

    updated league type and changed steamId to commId

--- models/lobby/server_test.go
+++ models/lobby/server_test.go
@@ -1,6 +1,8 @@
 package lobby
 
 import (
+	"errors"
+	"log"
 	"testing"
 
 	"github.com/TeamPlayTF/Server/config"
@@ -17,15 +19,30 @@ func TestServerSetup(t *testing.T) {
 		config.SetupConstants()
 		lobby.InitConfigs()
 
+		commId := "76561198067132047" // your commId, so it wont be kicking you out everytime
+
 		svr = lobby.NewServer()
 		svr.Map = "cp_process_final"
 		svr.Type = lobby.LobbyTypeHighlander
-		svr.League = "ugc"
-		svr.LobbyId = 0
+		svr.League = lobby.LeagueUgc
 		svr.Address = "192.168.1.94:27015"
 		svr.RconPassword = "rconPassword"
 		svr.LobbyPassword = "12345"
-		svr.AllowPlayer("[U:1:106866319]") // your steamId, so it wont be kicking you out everytime
-		svr.Setup()
+
+		svr.AllowPlayer(commId)
+
+		setupErr := svr.Setup()
+
+		if setupErr != nil {
+			log.Fatal(setupErr)
+		}
+
+		playerIsAllowed := svr.IsPlayerAllowed(commId)
+
+		if playerIsAllowed == false {
+			log.Fatal(errors.New("Error: your player isn't allowed in the server"))
+		}
+
+		svr.End()
 	}
 }
