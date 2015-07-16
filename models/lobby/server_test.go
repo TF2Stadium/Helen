2cb610512ceadb4bbebb96540ca0f4c4368a12e1
Sk1LLb0X <sk1llb0x123@gmail.com>
2015-07-16 18:20:15 -0300

    better tests fixed cycle in "go test ./.."

--- models/lobby/server_test.go
+++ models/lobby/server_test.go
@@ -1,30 +1,28 @@
 package lobby
 
 import (
-	"errors"
-	"log"
 	"testing"
 
 	"github.com/TeamPlayTF/Server/config"
-	"github.com/TeamPlayTF/Server/models/lobby"
+	"github.com/stretchr/testify/assert"
 )
 
 // change this if you wanna test the server
 // make sure you have the server running at the moment
 var shouldTest bool = false
-var svr *lobby.Server
+var svr *Server
 
 func TestServerSetup(t *testing.T) {
 	if shouldTest {
 		config.SetupConstants()
-		lobby.InitConfigs()
+		InitConfigs()
 
 		commId := "76561198067132047" // your commId, so it wont be kicking you out everytime
 
-		svr = lobby.NewServer()
+		svr = NewServer()
 		svr.Map = "cp_process_final"
-		svr.Type = lobby.LobbyTypeHighlander
-		svr.League = lobby.LeagueUgc
+		svr.Type = LobbyTypeHighlander
+		svr.League = LeagueUgc
 		svr.Address = "192.168.1.94:27015"
 		svr.RconPassword = "rconPassword"
 		svr.LobbyPassword = "12345"
@@ -32,16 +30,15 @@ func TestServerSetup(t *testing.T) {
 		svr.AllowPlayer(commId)
 
 		setupErr := svr.Setup()
-
-		if setupErr != nil {
-			log.Fatal(setupErr)
-		}
+		assert.Nil(t, setupErr, "Setup error")
 
 		playerIsAllowed := svr.IsPlayerAllowed(commId)
+		assert.True(t, playerIsAllowed, "Player isn't allowed, he should")
 
-		if playerIsAllowed == false {
-			log.Fatal(errors.New("Error: your player isn't allowed in the server"))
-		}
+		svr.DisallowPlayer(commId)
+
+		playerIsntAllowed := svr.IsPlayerAllowed(commId)
+		assert.False(t, playerIsntAllowed, "Player is allowed, he shouldn't")
 
 		svr.End()
 	}
