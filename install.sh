e58569d2b679a2e3a2b9cb33a885b392cd863aa9
Vibhav Pant <vibhavp@gmail.com>
2015-09-29 18:20:52 +0530

    Change install location.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 go build -v
 
 sudo cp ./etc/Helen.conf /etc/Helen.conf
-sudo cp ./etc/Helen.service /etc/systemd/user/
+sudo cp ./etc/Helen.service /usr/lib/systemd/system/
 sudo cp ./etc/runHelen /usr/bin/runHelen
 sudo cp ./Helen /usr/bin/Helen
 
