505886bb62580d5bf19e98b583c77d2eaa81ffe9
Vibhav Pant <vibhavp@gmail.com>
2015-12-09 17:42:34 +0530

    Register authority types

--- testhelpers/dbhelpers.go
+++ testhelpers/dbhelpers.go
@@ -5,13 +5,14 @@
 package testhelpers
 
 import (
-	//	"fmt"
+	"os"
+	"sync"
+
 	"github.com/TF2Stadium/Helen/config"
 	"github.com/TF2Stadium/Helen/config/stores"
 	"github.com/TF2Stadium/Helen/database"
 	"github.com/TF2Stadium/Helen/database/migrations"
-	"os"
-	"sync"
+	"github.com/TF2Stadium/Helen/helpers/authority"
 )
 
 var cleaningMutex sync.Mutex
@@ -19,13 +20,10 @@ var cleaningMutex sync.Mutex
 func CleanupDB() {
 	cleaningMutex.Lock()
 	defer cleaningMutex.Unlock()
-	if os.Getenv("DEPLOYMENT_ENV") == "" {
-		os.Setenv("DEPLOYMENT_ENV", "test")
-		defer os.Unsetenv("DEPLOYMENT_ENV")
-	}
+	os.Setenv("DEPLOYMENT_ENV", "test")
 	config.SetupConstants()
 	database.Init()
-
+	authority.RegisterTypes()
 	database.DB.Exec("DROP SCHEMA public CASCADE;")
 	database.DB.Exec("CREATE SCHEMA public;")
 
