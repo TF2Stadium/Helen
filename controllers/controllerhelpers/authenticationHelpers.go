c2b5ca35c744543580df2ca00d88e2c674bb7483
Vibhav Pant <vibhavp@gmail.com>
2015-12-16 16:01:31 +0530

    Remove unneeded code

--- controllers/controllerhelpers/authenticationHelpers.go
+++ controllers/controllerhelpers/authenticationHelpers.go
@@ -12,41 +12,8 @@ import (
 	"github.com/TF2Stadium/Helen/config/stores"
 	"github.com/TF2Stadium/Helen/helpers/authority"
 	"github.com/TF2Stadium/Helen/models"
-	"github.com/bitly/go-simplejson"
 	"github.com/gorilla/sessions"
 )
-
-func buildFakeSocketRequest(cookiesObj *simplejson.Json) *http.Request {
-	cookies, err := cookiesObj.Map()
-	if err != nil {
-		return &http.Request{}
-	}
-
-	str := ""
-
-	first := true
-	for k, v := range cookies {
-		vStr, ok := v.(string)
-		if !ok {
-			continue
-		}
-
-		if !first {
-			str += ";"
-		}
-		str += k + "=" + vStr
-		first = false
-	}
-
-	if str == "" {
-		return &http.Request{}
-	}
-
-	headers := http.Header{}
-	headers.Add("Cookie", str)
-
-	return &http.Request{Header: headers}
-}
 
 func AuthenticateSocket(socketid string, r *http.Request) error {
 	s, _ := GetSessionHTTP(r)
