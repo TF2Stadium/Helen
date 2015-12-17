11bba4448e46af0ee843d72916eb703871c749d7
Vibhav Pant <vibhavp@gmail.com>
2015-12-17 15:25:31 +0530

    Add coverage script

---
+++ cover.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+go test -coverprofile=handler.out ./controllers/socket/internal/handler/
+go tool cover -html=handler.out -o handler.html
+
+go test -coverprofile=models.out ./models/
+go tool cover -html=models.out -o models.html
+
+git clone git@github.com:TF2Stadium/coverage.git
+cp handler.html models.html ./coverage/
+cd coverage
+git add models.html handler.html
+git commit -m "Update coverage"
+git push -f
