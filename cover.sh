57c4d4a76b82aeb0b0474a0694d66a4f3301f01c
Vibhav Pant <vibhavp@gmail.com>
2015-12-17 15:40:52 +0530

    Add name, email

--- cover.sh
+++ cover.sh
@@ -9,6 +9,8 @@ go tool cover -html=models.out -o models.html
 git clone git@github.com:TF2Stadium/coverage.git
 cp handler.html models.html ./coverage/
 cd coverage
+git config --global user.email "this@is.bot"
+git config --global user.name "circleci deploy"
 git add models.html handler.html
 git commit -m "Update coverage"
 git push -f
