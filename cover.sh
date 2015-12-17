40f76c66dd4291458168e206a7f462d3105c91c4
Vibhav Pant <vibhavp@gmail.com>
2015-12-17 16:21:57 +0530

    Add last updated timestamp

--- cover.sh
+++ cover.sh
@@ -11,5 +11,7 @@ cp handler.html models.html ./coverage/
 cd coverage
 git config --global user.email "this@is.bot"
 git config --global user.name "circleci deploy"
-git add models.html handler.html
+cp index_template index.html
+printf "$(date -u) \n</body>" >> index.html
+git add models.html handler.html index.html
 git commit -m "Update coverage" && git push -f
