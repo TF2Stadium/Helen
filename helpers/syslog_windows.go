cf52355fc4d51f23bbfcca6b1a6eca96011b1068
Vibhav Pant <vibhavp@gmail.com>
2015-12-22 23:59:47 +0530

    Don't use syslog for windows

---
+++ helpers/syslog_windows.go
@@ -0,0 +1,13 @@
+//+build !linux
+
+// Copyright (C) 2015  TF2Stadium
+// Use of this source code is governed by the GPLv3
+// that can be found in the COPYING file.
+package helpers
+
+import (
+	"github.com/op/go-logging"
+)
+
+func setupPapertrail(_ string, _ logging.Backend) {
+}
