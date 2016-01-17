aca6f856eb19d5ac629e516ac95023ce95e4c23a
Vibhav Pant <vibhavp@gmail.com>
2016-01-17 14:38:24 +0530

    Don't test controller handlers

--- cover.sh
+++ cover.sh
@@ -3,7 +3,6 @@
 go get github.com/axw/gocov/gocov
 go get gopkg.in/matm/v1/gocov-html
 
-gocov test ./controllers/socket/internal/handler | gocov-html > handler.html
 gocov test ./models/ | gocov-html > models.html
 
 git clone git@github.com:TF2Stadium/coverage.git
