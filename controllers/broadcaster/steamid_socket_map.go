db0e0f6a9c51789f5b5bb4b4042c8c2270cff9c4
Vibhav Pant <vibhavp@gmail.com>
2016-01-17 10:47:11 +0530

    Add support for multiple tabs.
    Add steamIDSockets, socketSpectating.
    Remove SetSocket, replace with AddSocket.
    Make ChangeSocket work on steamIDSockets
    GetSockets now returns an array of wsevent Clients
    Add ConnectedSockets, SetSpectator, RemoveSpectator, IsSpectating

--- controllers/broadcaster/steamid_socket_map.go
+++ controllers/broadcaster/steamid_socket_map.go
@@ -6,32 +6,74 @@ import (
 	"github.com/TF2Stadium/wsevent"
 )
 
-var steamIdSocketMap = make(map[string]*wsevent.Client)
-var steamIdSocketMapLock = new(sync.RWMutex)
+var (
+	mapMu = new(sync.RWMutex)
+	//steamid -> client array, since players can have multiple tabs open
+	steamIDSockets = make(map[string][]*wsevent.Client)
+	//socketid -> id of lobby the socket is spectating
+	socketSpectating = make(map[string]uint)
+)
 
-func SetSocket(steamid string, so *wsevent.Client) {
-	steamIdSocketMapLock.Lock()
-	defer steamIdSocketMapLock.Unlock()
+func AddSocket(steamid string, so *wsevent.Client) {
+	mapMu.Lock()
+	defer mapMu.Unlock()
 
-	steamIdSocketMap[steamid] = so
+	steamIDSockets[steamid] = append(steamIDSockets[steamid], so)
 }
 
-func RemoveSocket(steamid string) {
-	steamIdSocketMapLock.Lock()
-	defer steamIdSocketMapLock.Unlock()
+func RemoveSocket(sessionID, steamID string) {
+	mapMu.Lock()
+	defer mapMu.Unlock()
 
-	delete(steamIdSocketMap, steamid)
+	clients := steamIDSockets[steamID]
+	for i, socket := range clients {
+		if socket.Id() == sessionID {
+			clients[i] = clients[len(clients)-1]
+			clients[len(clients)-1] = nil
+			clients = clients[:len(clients)-1]
+			break
+		}
+	}
+
+	steamIDSockets[steamID] = clients
+
+	if len(clients) == 0 {
+		delete(steamIDSockets, steamID)
+	}
 }
 
-func GetSocket(steamid string) (so *wsevent.Client, success bool) {
-	steamIdSocketMapLock.RLock()
-	defer steamIdSocketMapLock.RUnlock()
+func GetSockets(steamid string) (sockets []*wsevent.Client, success bool) {
+	mu.RLock()
+	defer mu.RUnlock()
 
-	so, success = steamIdSocketMap[steamid]
+	sockets, success = steamIDSockets[steamid]
 	return
 }
 
 func IsConnected(steamid string) bool {
-	_, ok := GetSocket(steamid)
+	_, ok := GetSockets(steamid)
+	return ok
+}
+
+func ConnectedSockets(steamid string) int {
+	return len(steamIDSockets[steamid])
+}
+
+func SetSpectator(socketID string, lobbyID uint) {
+	mapMu.Lock()
+	defer mapMu.Unlock()
+	socketSpectating[socketID] = lobbyID
+}
+
+func RemoveSpectator(socketID string) {
+	mapMu.Lock()
+	defer mapMu.Unlock()
+	delete(socketSpectating, socketID)
+}
+
+func IsSpectating(socketID string, lobbyID uint) bool {
+	mapMu.RLock()
+	defer mapMu.RUnlock()
+	_, ok := socketSpectating[socketID]
 	return ok
 }
