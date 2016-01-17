28f8f6b440bb6cbeb05c5e323097719a4f181823
Vibhav Pant <vibhavp@gmail.com>
2016-01-17 14:42:27 +0530

    Don't copy handler.html

--- cover.sh
+++ cover.sh
@@ -12,5 +12,5 @@ git config --global user.email "this@is.bot"
 git config --global user.name "circleci deploy"
 cp index_template index.html
 printf "$(date -u) \n</body>" >> index.html
-git add models.html handler.html index.html
+git add models.html index.html
 git commit -m "Update coverage" && git push -f
