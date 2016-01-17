94204e374f382fd23480a9244313ecb08c655ab4
Vibhav Pant <vibhavp@gmail.com>
2016-01-17 15:40:05 +0530

    Generate ban form with templates

--- controllers/admin/dir.go
+++ controllers/admin/dir.go
@@ -5,8 +5,11 @@
 package admin
 
 import (
+	"html/template"
 	"net/http"
 	"path/filepath"
+
+	"github.com/TF2Stadium/Helen/helpers"
 )
 
 func ServeAdminPage(w http.ResponseWriter, r *http.Request) {
@@ -14,9 +17,26 @@ func ServeAdminPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, abs)
 }
 
+type ban struct {
+	URL   string //URL
+	Title string //Title
+}
+
+var bans = []ban{
+	{"ban/join", "Ban from joining lobbies"},
+	{"ban/create", "Ban from creating lobbies"},
+	{"ban/chat", "Ban from chatting"},
+	{"ban/full", "Full ban"},
+}
+
 func ServeAdminBanPage(w http.ResponseWriter, r *http.Request) {
-	abs, _ := filepath.Abs("./views/admin/ban")
-	http.ServeFile(w, r, abs)
+	templ, err := template.ParseFiles("views/admin/templates/ban_forms.html")
+	if err != nil {
+		helpers.Logger.Error(err.Error())
+		return
+	}
+
+	templ.Execute(w, bans)
 }
 
 func ServeAdminRolePage(w http.ResponseWriter, r *http.Request) {
