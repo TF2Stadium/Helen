b9c6eaacca573636781db46c6c512a43c484ff6f
Vibhav Pant <vibhavp@gmail.com>
2016-01-18 21:20:47 +0530

    Run login tests in parallel

--- controllers/login/steamLogin_test.go
+++ controllers/login/steamLogin_test.go
@@ -26,6 +26,7 @@ func init() {
 }
 
 func TestLogin(t *testing.T) {
+	t.Parallel()
 	var count int
 
 	steamid := strconv.Itoa(rand.Int())
@@ -48,6 +49,7 @@ func TestLogin(t *testing.T) {
 }
 
 func TestWS(t *testing.T) {
+	t.Parallel()
 	steamid := strconv.Itoa(rand.Int())
 	client := testhelpers.NewClient()
 
