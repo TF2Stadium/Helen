35e8ff8e2e38e29bea2fdc9aa8aea4deed9f6d5d
Vibhav Pant <vibhavp@gmail.com>
2016-01-22 03:56:20 +0530

    Use DefaultHandler function

--- routes/socket/socket.go
+++ routes/socket/socket.go
@@ -1,8 +1,6 @@
 package socket
 
 import (
-	"encoding/json"
-
 	"github.com/TF2Stadium/Helen/helpers"
 	"github.com/TF2Stadium/Helen/routes/socket/middleware"
 	"github.com/TF2Stadium/wsevent"
@@ -11,27 +9,12 @@ import (
 var (
 	//AuthServer is the wsevent server where authenticated (logged in) users/sockets
 	//are added to
-	AuthServer = wsevent.NewServer(middleware.JSONCodec{})
+	AuthServer = wsevent.NewServer(middleware.JSONCodec{}, func(_ *wsevent.Client, _ struct{}) interface{} {
+		return helpers.NewTPError("No such request.", -3)
+	})
 	//UnauthServer is the wsevent server where unauthenticated users/sockets
 	//are added to
-	UnauthServer = wsevent.NewServer(middleware.JSONCodec{})
-)
-
-func InitializeSocketServer() {
-	AuthServer.DefaultHandler = func(_ *wsevent.Client, _ []byte) interface{} {
-		return helpers.NewTPError("No such request.", -3)
-	}
-
-	UnauthServer.DefaultHandler = func(_ *wsevent.Client, data []byte) interface{} {
+	UnauthServer = wsevent.NewServer(middleware.JSONCodec{}, func(_ *wsevent.Client, _ struct{}) interface{} {
 		return helpers.NewTPError("Player isn't logged in.", -4)
-	}
-
-}
-
-func getEvent(data []byte) string {
-	var js struct {
-		Request string
-	}
-	json.Unmarshal(data, &js)
-	return js.Request
-}
+	})
+)
