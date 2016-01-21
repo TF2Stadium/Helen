4ebba8449892eeed647fc371e29c8dcf45cc1d0c
Vibhav Pant <vibhavp@gmail.com>
2016-01-22 03:28:46 +0530

    Move GetPArams to JSONCodec middleware

--- controllers/controllerhelpers/filters.go
+++ controllers/controllerhelpers/filters.go
@@ -5,14 +5,9 @@
 package controllerhelpers
 
 import (
-	"encoding/json"
 	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"reflect"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -101,73 +96,4 @@ func FilterHTTPRequest(action authority.AuthAction, f func(http.ResponseWriter, 
 
 		f(w, r)
 	}
-}
-
-//I forgot to document this while working on it, so it might be a bit
-//difficult to understand what's going on.
-//THINK TWICE BEFORE CHANGING ANYTHING HERE
-func GetParams(data []byte, v interface{}) error {
-	err := json.Unmarshal(data, v)
-
-	if err != nil {
-		return err
-	}
-
-	stValue := reflect.Indirect(reflect.ValueOf(v))
-	stType := stValue.Type()
-
-outer:
-	for i := 0; i < stType.NumField(); i++ {
-		field := stType.Field(i)
-		fieldPtrValue := stValue.Field(i) //The pointer field
-
-		if fieldPtrValue.Type().Elem().Kind() != reflect.String {
-			if fieldPtrValue.IsNil() && field.Tag.Get("empty") == "" {
-				return fmt.Errorf(`Field "%s" cannot be null`, strings.ToLower(field.Name))
-			}
-		} else if fieldPtrValue.IsNil() {
-			empty := field.Tag.Get("empty")
-			if empty == "-" {
-				empty = ""
-			} else {
-				return fmt.Errorf(`Field "%s" cannot be null`, strings.ToLower(field.Name))
-			}
-			fieldPtrValue.Set(reflect.ValueOf(&empty))
-		}
-
-		validTag := field.Tag.Get("valid")
-		if validTag == "" {
-			continue
-		}
-
-		fieldValue := reflect.Indirect(fieldPtrValue) //The value to which the pointer points too
-		validValues := strings.Split(validTag, ",")
-		var valid bool
-
-		for _, validVal := range validValues {
-			switch fieldValue.Kind() {
-			case reflect.Uint:
-				num, err := strconv.ParseUint(validVal, 2, 32)
-				if err != nil {
-					panic(fmt.Errorf("Error while parsing struct tag: %s",
-						err.Error()))
-				}
-
-				if reflect.DeepEqual(fieldValue.Uint(), num) {
-					continue outer
-				}
-
-			case reflect.String:
-				if reflect.DeepEqual(fieldValue.String(), validVal) {
-					continue outer
-				}
-
-			}
-		}
-		if !valid {
-			return fmt.Errorf("Field %s isn't valid.", field.Name)
-		}
-	}
-
-	return nil
 }
---
+++ routes/socket/middleware/codec.go
@@ -0,0 +1,68 @@
+package middleware
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/TF2Stadium/Helen/helpers"
+)
+
+type JSONCodec struct{}
+
+func (JSONCodec) ReadName(data []byte) string {
+	var body struct {
+		Request string
+	}
+	json.Unmarshal(data, &body)
+	return body.Request
+}
+
+func (JSONCodec) Unmarshal(data []byte, v interface{}) error {
+	err := json.Unmarshal(data, v)
+
+	if err != nil {
+		return err
+	}
+
+	stValue := reflect.Indirect(reflect.ValueOf(v))
+
+outer:
+	for i := 0; i < stValue.NumField(); i++ {
+		curField := stValue.Type().Field(i)
+		fieldPtrValue := stValue.Field(i) //The pointer field
+
+		if fieldPtrValue.IsNil() {
+			if curField.Tag.Get("empty") == "" {
+				return fmt.Errorf(`Field "%s" cannot be null`, strings.ToLower(curField.Name))
+			}
+
+			if fieldPtrValue.Type().Elem().Kind() == reflect.String {
+				blank := ""
+				fieldPtrValue.Set(reflect.ValueOf(&blank))
+			}
+		}
+
+		//If a list of valid strings is supplied, check if the field
+		//is valid
+		validTag := curField.Tag.Get("valid")
+		if validTag == "" {
+			continue
+		}
+
+		for _, valid := range strings.Split(validTag, ",") {
+			if reflect.DeepEqual(reflect.Indirect(fieldPtrValue).String(), valid) {
+				continue outer
+			}
+		}
+		return fmt.Errorf("Field %s isn't valid.", curField.Name)
+	}
+
+	return nil
+}
+
+func (JSONCodec) MarshalError(err error) []byte {
+	bytes, _ := json.Marshal(helpers.NewTPErrorFromError(err))
+	return bytes
+}
