7a17442a1ca1f03c61c61096edb37e0ff8a86732
Vibhav Pant <vibhavp@gmail.com>
2016-02-08 17:44:08 +0530

    Remote PlayerSetting

--- database/migrations/migrations.go
+++ database/migrations/migrations.go
@@ -20,12 +20,10 @@ func Do() {
 	database.DB.AutoMigrate(&models.LobbySlot{})
 	database.DB.AutoMigrate(&models.ServerRecord{})
 	database.DB.AutoMigrate(&models.PlayerStats{})
-	database.DB.AutoMigrate(&models.PlayerSetting{})
 	database.DB.AutoMigrate(&models.AdminLogEntry{})
 	database.DB.AutoMigrate(&models.PlayerBan{})
 
 	database.DB.Model(&models.LobbySlot{}).AddUniqueIndex("idx_lobby_slot_lobby_id_slot", "lobby_id", "slot")
-	database.DB.Model(&models.PlayerSetting{}).AddUniqueIndex("idx_player_id_key", "player_id", "key")
 	database.DB.AutoMigrate(&models.ChatMessage{})
 	database.DB.AutoMigrate(&models.Requirement{})
 	database.DB.AutoMigrate(&Constant{})
