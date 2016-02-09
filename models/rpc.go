a09e5ab907b02e709d6f23df37709a7af73e33fd
Vibhav Pant <vibhavp@gmail.com>
2016-02-09 21:11:46 +0530

    ConnectRPC: lookup hosts on etcd

--- models/rpc.go
+++ models/rpc.go
@@ -5,8 +5,11 @@
 package models
 
 import (
+	"strings"
+
 	"github.com/Sirupsen/logrus"
 	"github.com/TF2Stadium/Helen/config"
+	"github.com/TF2Stadium/Helen/helpers"
 	"github.com/vibhavp/rpcconn"
 )
 
@@ -17,14 +20,34 @@ var (
 
 func ConnectRPC() {
 	var err error
+	var addr string
 
 	if config.Constants.PaulingAddr != "" {
-		pauling, err = rpcconn.DialHTTP("tcp", config.Constants.PaulingAddr)
+		addr = config.Constants.PaulingAddr
+
+		if strings.HasPrefix(addr, "etcd:") {
+			addr, err = helpers.GetAddr(strings.Split(addr, ":")[1])
+			if err != nil {
+				logrus.Fatal(err)
+			}
+		}
+
+		pauling, err = rpcconn.DialHTTP("tcp", addr)
 		if err != nil {
 			logrus.Fatal(err)
 		}
 	}
+
 	if config.Constants.FumbleAddr != "" {
+		addr = config.Constants.FumbleAddr
+
+		if strings.HasPrefix(addr, "etcd:") {
+			addr, err = helpers.GetAddr(strings.Split(addr, ":")[1])
+			if err != nil {
+				logrus.Fatal(err)
+			}
+		}
+
 		fumble, err = rpcconn.DialHTTP("tcp", config.Constants.FumbleAddr)
 		if err != nil {
 			logrus.Fatal(err)
