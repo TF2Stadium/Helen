34f46666fcf9f75bfbae164780595037515f2604
Vibhav Pant <vibhavp@gmail.com>
2016-02-13 21:22:28 +0530

    Use templates to show more player info.

--- controllers/static.go
+++ controllers/static.go
@@ -8,8 +8,22 @@ import (
 	"fmt"
 	"net/http"
 
+	"html/template"
+
 	"github.com/TF2Stadium/Helen/controllers/controllerhelpers"
+	"github.com/TF2Stadium/Helen/models"
 )
+
+var playerTempl = template.Must(template.New("player").Parse(
+	`
+<html>
+  <body>
+    <a href="/logout">Logout here</a> <br>
+    Logged in as {{.Name}} ({{.SteamID}}) on Steam <br>
+    {{if .TwitchName}} Twitch connect connected - <a href="http://twitch.tv/{{.TwitchName}}">{{.TwitchName}}</a> {{end}} <br>
+  </body>
+</html>
+`))
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -20,9 +34,10 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 	if controllerhelpers.IsLoggedInHTTP(r) {
 		session, _ := controllerhelpers.GetSessionHTTP(r)
 		var steamid = session.Values["steam_id"].(string)
-		fmt.Fprintf(w, `<html><head></head><body>hello! You're logged in and your steam id is
-			`+steamid+`. You can log out <a href='/logout'>here</a></body></html>`)
+
+		player, _ := models.GetPlayerBySteamID(steamid)
+		playerTempl.Execute(w, player)
 	} else {
-		fmt.Fprintf(w, "<html><head></head><body>hello! You can log in <a href='/startLogin'>here</a></body></html>")
+		fmt.Fprintf(w, `<html><head></head><body>hello! You can log in <a href='/startLogin'>here</a></body></html>`)
 	}
 }
