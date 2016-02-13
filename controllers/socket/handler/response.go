baa2b676ed2c32c0c2cfe4863b0c0a1c11110bdd
Vibhav Pant <vibhavp@gmail.com>
2016-02-13 18:49:57 +0530

    Move responses code to handler package

--- controllers/socket/handler/response.go
+++ controllers/socket/handler/response.go
@@ -2,24 +2,18 @@
 // Use of this source code is governed by the GPLv3
 // that can be found in the COPYING file.
 
-package controllerhelpers
-
-import "encoding/json"
+package handler
 
 //Response stores a successful response to a RPC call
-type Response struct {
+type response struct {
 	Success bool        `json:"success"`
 	Data    interface{} `json:"data"`
 }
 
 //Create a new response
-func NewResponse(data interface{}) Response {
-	return Response{true, data}
-}
-
-func (r Response) Encode() ([]byte, error) {
-	return json.Marshal(r)
+func newResponse(data interface{}) response {
+	return response{true, data}
 }
 
 //EmptySuccessJS is the empty success response
-var EmptySuccessJS = NewResponse(struct{}{})
+var emptySuccess = newResponse(struct{}{})
