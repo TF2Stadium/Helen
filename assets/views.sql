13cdd2a29cfd696fd3f07f41bba885de77803dc2
Vibhav Pant <vibhavp@gmail.com>
2016-02-20 21:03:48 +0530

    Add player's mumble username, authkey and ID to view

--- assets/views.sql
+++ assets/views.sql
@@ -5,7 +5,10 @@ SELECT players.steam_id,
        lobby_slots.slot,
        lobby_slots.lobby_id,
        lobby_slots.needs_sub,
-       lobbies.state
+       lobbies.state,
+       players.mumble_username,
+       players.mumble_authkey,
+       players.id
 FROM players
 INNER JOIN lobby_slots ON lobby_slots.player_id = players.id
 INNER JOIN lobbies ON lobby_slots.lobby_id = lobbies.id;
