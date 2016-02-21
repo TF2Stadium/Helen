be5e4dea7a3b8618f98db09e6e462301a3bfd197
Vibhav Pant <vibhavp@gmail.com>
2016-02-22 01:39:24 +0530

    Remove unused functions.

--- models/fumble.go
+++ models/fumble.go
@@ -1,14 +1,13 @@
 package models
 
-import (
-	"strings"
+import "github.com/Sirupsen/logrus"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/TF2Stadium/fumble/mumble"
-)
+func fumbleLobbyCreated(lobbyID uint) error {
+	if !*fumbleEnabled {
+		return nil
+	}
 
-func FumbleLobbyCreated(lob *Lobby) error {
-	err := fumble.Call("Fumble.CreateLobby", lob.ID, &struct{}{})
+	err := fumble.Call("Fumble.CreateLobby", lobbyID, nil)
 
 	if err != nil {
 		logrus.Error(err)
@@ -18,32 +17,12 @@ func FumbleLobbyCreated(lob *Lobby) error {
 	return nil
 }
 
-func fumbleAllowPlayer(lobbyId uint, playerName string, playerTeam string) error {
-	user := mumble.User{}
-	user.Name = playerName
-	user.Team = mumble.Team(playerTeam)
-
-	err := fumble.Call("Fumble.AllowPlayer", &mumble.LobbyArgs{
-		User: user, LobbyID: lobbyId}, &struct{}{})
-	if err != nil {
-		logrus.Error(err)
+func fumbleLobbyEnded(lobbyID uint) {
+	if !*fumbleEnabled {
+		return
 	}
 
-	return nil
-}
-
-func FumbleLobbyPlayerJoinedSub(lob *Lobby, player *Player, slot int) {
-	team, class, _ := LobbyGetSlotInfoString(lob.Type, slot)
-	fumbleAllowPlayer(lob.ID, strings.ToUpper(team)+"_"+strings.ToUpper(class), strings.ToUpper(team))
-}
-
-func FumbleLobbyPlayerJoined(lob *Lobby, player *Player, slot int) {
-	team, class, _ := LobbyGetSlotInfoString(lob.Type, slot)
-	fumbleAllowPlayer(lob.ID, strings.ToUpper(team)+"_"+strings.ToUpper(class), "")
-}
-
-func FumbleLobbyEnded(lob *Lobby) {
-	err := fumble.Call("Fumble.EndLobby", lob.ID, nil)
+	err := fumble.Call("Fumble.EndLobby", lobbyID, nil)
 	if err != nil {
 		logrus.Error(err)
 	}
