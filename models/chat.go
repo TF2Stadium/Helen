d72fb88e989879fc0ef0a9283bedf0079a3ab601
Vibhav Pant <vibhavp@gmail.com>
2016-03-03 19:47:39 +0530

    Implement MarshalJSON for ChatMessage/sentMessage

--- models/chat.go
+++ models/chat.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"encoding/json"
 	"github.com/TF2Stadium/Helen/controllers/broadcaster"
 	db "github.com/TF2Stadium/Helen/database"
 )
@@ -13,31 +14,24 @@ type ChatMessage struct {
 	// Message ID
 	ID        uint      `json:"id"`
 	CreatedAt time.Time `json:"timestamp"`
-
-	Player   Player `json:"player"` // not in the DB, used by frontend to retrieve player information
-	PlayerID uint   `json:"-"`      // ID of the player who sent the message
-
-	// Room to which the message was sent
-	Room int `json:"room"`
-	// The actual Message, limited to 150 characters
-	Message string `json:"message" sql:"type:varchar(150)"`
-	// True if the message has been deleted by a moderator
-	Deleted bool `json:"deleted"`
-	// true if the message is sent by a bot
-	Bot bool `json:"bot"`
-	// true if the message is in-game
-	InGame bool `json:"ingame"`
+	Player    Player    `json:"-"`
+	PlayerID  uint      `json:"-"`                               // ID of the player who sent the message
+	Room      int       `json:"room"`                            // room to which the message was sent
+	Message   string    `json:"message" sql:"type:varchar(150)"` // the actual Message
+	Deleted   bool      `json:"deleted"`                         // true if the message has been deleted by a moderator
+	Bot       bool      `json:"bot"`                             // true if the message was sent by the notification "bot"
+	InGame    bool      `json:"ingame"`                          // true if the message is in-game
 }
 
-var botSummary = Player{
+var botPlayer = Player{
 	JSONFields: JSONFields{
 		PlaceholderTags: new([]string),
 	},
 }
 
 func init() {
-	botSummary.Name = "TF2Stadium"
-	*botSummary.PlaceholderTags = []string{"tf2stadium"}
+	botPlayer.Name = "TF2Stadium"
+	*botPlayer.PlaceholderTags = []string{"tf2stadium"}
 }
 
 // Return a new ChatMessage sent from specficied player
@@ -45,7 +39,6 @@ func NewChatMessage(message string, room int, player *Player) *ChatMessage {
 	player.SetPlayerSummary()
 	record := &ChatMessage{
 		PlayerID: player.ID,
-		Player:   *player,
 
 		Room:    room,
 		Message: message,
@@ -57,7 +50,6 @@ func NewChatMessage(message string, room int, player *Player) *ChatMessage {
 func NewInGameChatMessage(lobby *Lobby, player *Player, message string) *ChatMessage {
 	return &ChatMessage{
 		PlayerID: player.ID,
-		Player:   *player,
 
 		Room:    int(lobby.ID),
 		Message: message,
@@ -68,15 +60,51 @@ func NewInGameChatMessage(lobby *Lobby, player *Player, message string) *ChatMes
 func (m *ChatMessage) Save() { db.DB.Save(m) }
 
 func (m *ChatMessage) Send() {
-	broadcaster.SendMessageToRoom(fmt.Sprintf("%d_public", m.Room), "chatReceive", m)
+	broadcaster.SendMessageToRoom(fmt.Sprintf("%d_public", m.Room), "chatReceive", (*sentMessage)(m))
 	if m.Room != 0 {
-		broadcaster.SendMessageToRoom(fmt.Sprintf("%d_private", m.Room), "chatReceive", m)
+		broadcaster.SendMessageToRoom(fmt.Sprintf("%d_private", m.Room), "chatReceive", (*sentMessage)(m))
 	}
+}
+
+// we only need these three things for showing player messages
+type minPlayer struct {
+	Name    string   `json:"name"`
+	SteamID string   `json:"steamid"`
+	Tags    []string `json:"tags"`
+}
+
+// sentMessage aliases ChatMessage, since implementing MarshalJSON for
+// ChatMessage would result in a recursive data structure (see below),
+// which the json parser cannot marshal
+type sentMessage ChatMessage
+
+func (m *sentMessage) MarshalJSON() ([]byte, error) {
+	var player *Player
+	if m.Bot {
+		player = &botPlayer
+	} else {
+		player, _ = GetPlayerByID(m.PlayerID)
+	}
+
+	message := struct {
+		*ChatMessage
+		Player *minPlayer `json:"player"`
+	}{(*ChatMessage)(m), &minPlayer{
+		Name:    player.Alias(),
+		SteamID: player.SteamID,
+		Tags:    decoratePlayerTags(player),
+	}}
+
+	if m.Deleted {
+		message.Message = "<deleted>"
+		message.Player.Tags = append(message.Player.Tags, "deleted")
+	}
+
+	return json.Marshal(message)
 }
 
 func NewBotMessage(message string, room int) *ChatMessage {
 	m := &ChatMessage{
-		Player:  botSummary,
 		Room:    room,
 		Message: message,
 
@@ -112,22 +140,10 @@ func GetPlayerMessages(player *Player) ([]*ChatMessage, error) {
 }
 
 // Get a list of last 20 messages sent to room, used by frontend for displaying the chat history/scrollback
-func GetScrollback(room int) ([]*ChatMessage, error) {
-	var messages []*ChatMessage
+func GetScrollback(room int) ([]*sentMessage, error) {
+	var messages []*sentMessage // apparently the ORM works fine with using this type (they're aliases after all)
 
 	err := db.DB.Table("chat_messages").Where("room = ? AND deleted = FALSE", room).Order("id desc").Limit(20).Find(&messages).Error
 
-	for _, message := range messages {
-		var player Player
-		if message.Bot {
-			message.Player = botSummary
-		} else {
-			db.DB.First(&player, message.PlayerID)
-			player.PlaceholderTags = new([]string)
-			*player.PlaceholderTags = decoratePlayerTags(&player)
-
-			message.Player = player
-		}
-	}
 	return messages, err
 }
