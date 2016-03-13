46faf8b6af25335abd38b48c79fbbfc3b6520b77
Vibhav Pant <vibhavp@gmail.com>
2016-03-13 19:27:18 +0530

    Initialize template for stored servers

--- controllers/admin/templates.go
+++ controllers/admin/templates.go
@@ -5,6 +5,7 @@ import (
 )
 
 func InitAdminTemplates() {
+	serverPage = template.Must(template.ParseFiles("views/admin/templates/server.html"))
 	banlogsTempl = template.Must(template.ParseFiles("views/admin/templates/ban_logs.html"))
 	chatLogsTempl = template.Must(template.ParseFiles("views/admin/templates/chatlogs.html"))
 	lobbiesTempl = template.Must(template.ParseFiles("views/admin/templates/lobbies.html"))
