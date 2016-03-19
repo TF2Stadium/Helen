c550079ce89154b04f8efb217dd728764684adf9
Vibhav Pant <vibhavp@gmail.com>
2016-03-19 17:32:48 +0530

    Use travis

--- internal/testhelpers/dbhelpers.go
+++ internal/testhelpers/dbhelpers.go
@@ -25,14 +25,13 @@ func CleanupDB() {
 	o.Do(func() {
 		config.SetupConstants()
 
-		circleci := os.Getenv("CIRCLECI")
 		ci := os.Getenv("CI")
 		config.Constants.DbAddr = "127.0.0.1:5432"
 		config.Constants.SteamDevAPIKey = ""
 
-		if circleci == "true" && ci == "true" {
-			config.Constants.DbUsername = "ubuntu"
-			config.Constants.DbDatabase = "circle_test"
+		if ci == "true" {
+			config.Constants.DbUsername = "postgres"
+			config.Constants.DbDatabase = "travis_ci_test"
 			config.Constants.DbPassword = ""
 		} else {
 			config.Constants.DbDatabase = "TESTtf2stadium"
