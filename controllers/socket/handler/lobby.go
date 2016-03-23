f4da43202c097c84a099d50cc5deaf8e3e0b5b17
Vibhav Pant <vibhavp@gmail.com>
2016-03-23 20:32:38 +0530

    Allow mods to reset server

--- controllers/socket/handler/lobby.go
+++ controllers/socket/handler/lobby.go
@@ -319,8 +319,8 @@ func (Lobby) LobbyServerReset(so *wsevent.Client, args struct {
 	player := chelpers.GetPlayer(so.Token)
 	lobby, tperr := models.GetLobbyByID(*args.ID)
 
-	if player.SteamID != lobby.CreatedBySteamID || player.Role != helpers.RoleAdmin {
-		return errors.New("Player not authorized to reset server.")
+	if player.SteamID != lobby.CreatedBySteamID || (player.Role != helpers.RoleAdmin && player.Role != helpers.RoleMod) {
+		return errors.New("You are not authorized to reset server.")
 	}
 
 	if tperr != nil {
