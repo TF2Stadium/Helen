f46dce7d30baeedb2c25409f48796b746acf1b76
Vibhav Pant <vibhavp@gmail.com>
2016-04-13 09:48:46 +0530

    Clear all tables

--- internal/testhelpers/dbhelpers.go
+++ internal/testhelpers/dbhelpers.go
@@ -12,11 +12,30 @@ import (
 	"github.com/TF2Stadium/Helen/database"
 	"github.com/TF2Stadium/Helen/database/migrations"
 	_ "github.com/TF2Stadium/Helen/helpers/authority"
+	"github.com/TF2Stadium/Helen/models"
+	"github.com/TF2Stadium/Helen/models/chat"
+	"github.com/TF2Stadium/Helen/models/gameserver"
+	"github.com/TF2Stadium/Helen/models/lobby"
+	"github.com/TF2Stadium/Helen/models/player"
 )
 
-var cleaningMutex sync.Mutex
-
-var o = new(sync.Once)
+var (
+	cleaningMutex sync.Mutex
+	o             = new(sync.Once)
+	tables        = []interface{}{
+		&player.Player{},
+		&lobby.Lobby{},
+		&lobby.LobbySlot{},
+		&gameserver.ServerRecord{},
+		&player.PlayerStats{},
+		&models.AdminLogEntry{},
+		&player.PlayerBan{},
+		&chat.ChatMessage{},
+		&lobby.Requirement{},
+		&gameserver.StoredServer{},
+		&player.Report{},
+	}
+)
 
 func CleanupDB() {
 	cleaningMutex.Lock()
@@ -38,9 +57,10 @@ func CleanupDB() {
 		}
 
 		database.Init()
+		for _, table := range tables {
+			database.DB.DropTable(table)
+		}
+		migrations.Do()
 	})
 
-	database.DB.Exec("drop schema public cascade")
-	database.DB.Exec("create schema public")
-	migrations.Do()
 }
