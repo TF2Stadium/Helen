cae5286a562c9dfa5953cd338631a7317d3d9747
gpittarelli <tf2stadium@gjp.cc>
2016-10-30 16:32:51 -0700

    Add lobbyShuffle endpoint

--- controllers/socket/handler/lobby.go
+++ controllers/socket/handler/lobby.go
@@ -921,3 +921,22 @@ func (Lobby) LobbyRemoveRegionLock(so *wsevent.Client, args struct {
 	return emptySuccess
 
 }
+
+func (Lobby) LobbyShuffle(so *wsevent.Client, args struct {
+	Id uint `json:"id"`
+}) interface{} {
+	player := chelpers.GetPlayer(so.Token)
+
+	lob, err := lobby.GetLobbyByID(args.Id)
+	if err != nil {
+		return err
+	}
+
+	if player.SteamID != lob.CreatedBySteamID && (player.Role != helpers.RoleAdmin && player.Role != helpers.RoleMod) {
+		return errors.New("You aren't authorized to shuffle this lobby.")
+	}
+
+	lob.ShuffleAllSlots()
+	lob.Save()
+	return emptySuccess
+}
