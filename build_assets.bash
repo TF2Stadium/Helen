b075a4c23d306f238f3329fbd6dc208cb1ff2beb
George Pittarelli <tf2stadium@gjp.cc>
2020-04-03 01:23:09 -0400

    Handle maxmind download being a .tar.gz instead of just .gz

--- build_assets.bash
+++ build_assets.bash
@@ -7,8 +7,12 @@ function download_assets {
     fi
 
     echo "Downloading assets"
-    curl -o "assets/geoip.mmdb.gz" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${MAXMIND_API_KEY}&suffix=tar.gz"
-    gzip -d -f assets/geoip.mmdb.gz
+    pushd assets
+    curl -o "geoip.tar.gz" "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${MAXMIND_API_KEY}&suffix=tar.gz"
+    tar xzvf geoip.tar.gz --wildcards --no-anchored '*.mmdb' --strip-components=1
+    mv GeoLite2-Country.mmdb geoip.mmdb
+    rm geoip.tar.gz
+    popd
 }
 
 if [ ! -f assets/geoip.mmdb ]; then
@@ -19,11 +23,11 @@ if [ "$1" = "production" ]; then
     echo "Compiling assets"
     go-bindata -nomemcopy -ignore="bindata\.go" \
 	       -pkg assets 		        \
-	       -o assets/bindata.go assets/
+	       -o assets/bindata.go assets/{geoip.mmdb,lobbySettingsData.json}
 else
     echo "Compiling assets (debug)"
     go-bindata -debug -ignore="bindata\.go" \
 	       -pkg assets    		    \
-	       -o assets/bindata.go assets/
+	       -o assets/bindata.go assets/{geoip.mmdb,lobbySettingsData.json}
 
 fi
